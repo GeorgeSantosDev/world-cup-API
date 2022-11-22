1b40fafc2db04033ae3199d9de40b3c0c81f45e4
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-21 22:52:25 -0300

    feat: Create players service functions

---
+++ src/services/players.service.js
@@ -0,0 +1,40 @@
+const { playersModel } = require('../models');
+
+const findAllPlayers = async () => {
+  const players = await playersModel.findAll();
+
+  if (players) return { type: null, message: players };
+
+  return { type: 'INTERNAL_ERROR', message: 'Internal error' };
+};
+
+const findPlayerById = async (id) => {
+  const player = await playersModel.findById(id);
+
+  if (player) return { type: null, message: player };
+
+  return { type: 'PLAYER_NOT_FOUND', message: 'Player not found' };
+};
+
+const findPlayerByName = async (name) => {
+  const player = await playersModel.findByName(name);
+
+  if (player) return { type: null, message: player };
+
+  return { type: 'PLAYER_NOT_FOUND', message: 'Player not found' };
+};
+
+const findPlayerByCountryId = async (countryId) => {
+  const player = await playersModel.findByCountryId(countryId);
+
+  if (player) return { type: null, message: player };
+
+  return { type: 'PLAYER_NOT_FOUND', message: 'Player not found' };
+};
+
+module.exports = {
+  findAllPlayers,
+  findPlayerById,
+  findPlayerByName,
+  findPlayerByCountryId,
+};
